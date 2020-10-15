c9596a58f7fbd4778d639e90c35dcd492cabd771
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-10-15 12:57:41 +0000

    init

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import Wrap from "firejsx/Wrap";
+import {hot} from "firejsx/Hot";
+
+Wrap(() => {
+    return (
+        <div>
+            <h1>My first FireJSX web page</h1>
+        </div>
+    )
+},hot)
